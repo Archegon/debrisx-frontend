22cc89434991b618131e952d384b3a757b744a89
nircniv <ng.ta.wei@outlook.com>
2024-05-19 06:47:34 +0800

    feat: Add chroma-js and react-pro-sidebar packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import { ColorModeContext, useMode } from './theme';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Box, CssBaseline, ThemeProvider } from '@mui/material';
 import Topbar from './scenes/global/Topbar';
 import Dashboard from './scenes/dashboard';
+import FAQ from './scenes/faq';
+import Sidebar from './scenes/global/Sidebar';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -13,10 +15,16 @@ function App() {
         <CssBaseline />
         <div className="app">
           <main className="content">
-            <Topbar />
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-            </Routes>
+            <Sidebar />
+            <Box flexGrow={1} display={'flex'} flexDirection={'column'}>
+              <Topbar />
+              <Box px={3}>
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/faq" element={<FAQ />} />
+                </Routes>
+              </Box>
+            </Box>
           </main>
         </div>
       </ThemeProvider>
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,5 +1,50 @@
+import { Sidebar as ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { NavLink } from 'react-router-dom';
+import { tokens } from '../../theme';
+import { Box, Divider, Typography, useTheme } from '@mui/material';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import QuizIcon from '@mui/icons-material/Quiz';
+import SettingsIcon from '@mui/icons-material/Settings';
+
 const Sidebar = () => {
-    return <div>Sidebar</div>
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return (
+        <ProSidebar
+            backgroundColor={theme.palette.mode === "dark" ? "transparent" : colors.primary[400]}
+            rootStyles={{
+                border: 'None',
+                background: `linear-gradient(to bottom, ${colors.primary[400]}, ${colors.primary[600]})`,
+            }}
+        >
+            <Box px={2} pt={3}>
+                <Typography align='center' fontWeight={'bold'} variant='h2'>
+                    Debris <span style={{ color: 'orange' }}>X</span><br />
+                    <Typography variant='subtitle1'>Control Panel</Typography>
+                </Typography>
+                <Divider sx={{ pt: 3 }} />
+                <Menu
+                    menuItemStyles={{
+                        button: {
+                            [`&.active`]: {
+                                fontWeight: 'bold',
+                                color: colors.blueAccent[500],
+                            },
+                            '&:hover': {
+                                color: colors.blueAccent[500],
+                                backgroundColor: 'transparent',
+                            },
+                        },
+                    }}
+                >
+                    <MenuItem icon={<DashboardIcon />} component={<NavLink to="/" />}>Dashboard</MenuItem>
+                    <MenuItem icon={<QuizIcon />} component={<NavLink to="/faq" />}>FAQ</MenuItem>
+                    <MenuItem icon={<SettingsIcon />}>Settings</MenuItem>
+                </Menu>
+            </Box>
+        </ProSidebar>
+    );
 }
 
 export default Sidebar;
